0dc3128a5ebc7428a434341f785e40373745798e
spiritofenergy <spiritofenergy220@gmail.com>
2022-10-05 10:57:45 +0300

    Lesson-123

---
+++ app/src/main/java/com/kodexgroop/nawsapp/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,24 @@
+package com.example.nawsapp.ui.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.nawsapp.R
+import com.example.nawsapp.databinding.FragmentDetailsBinding
+import com.example.nawsapp.databinding.FragmentFavoriteBinding
+
+
+class FavoriteFragment : Fragment() {
+    private var _binding: FragmentFavoriteBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
